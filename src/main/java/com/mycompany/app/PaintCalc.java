e8ca2461eaab1d17b37826d2104012cd472f9a6b
shakib108 <86781389+shakib108@users.noreply.github.com>
2021-11-18 03:46:41 +0000

    volume() now functions properly
    All functions work correctly. All tests pass.. Also added JavaDoc annotations.

--- src/main/java/com/mycompany/app/PaintCalc.java
+++ src/main/java/com/mycompany/app/PaintCalc.java
@@ -1,12 +1,29 @@
 package com.mycompany.app;
 
+/**
+ Class that contains functions to calculate various properties of a room.
+ Assumptions: Room is a simple rectangular prism
+ */
 public class PaintCalc {
 
+    /**
+     * Calculate area of the room's floor
+     * @param length of room
+     * @param width of room
+     * @return area of floor
+     */
     public static float area(float length, float width)
     {
         return length * width;
     }
 
+    /**
+     * Calculate surface area of the walls of the room.
+     * @param length of room
+     * @param width of room
+     * @param height of room
+     * @return surface area of walls
+     */
     public static float surfaceArea(float length, float width, float height)
     {
         float longWalls = 2 * (length * height);
@@ -15,10 +32,16 @@ public class PaintCalc {
         return longWalls + shortWalls;
     }
 
+    /**
+     * Calculate volume of the room
+     * @param length of room
+     * @param width of room
+     * @param height of room
+     * @return volume of room
+     */
     public static float volume(float length, float width, float height)
     {
-
-        return 0;
+        return length * width * height;
     }
 
 
